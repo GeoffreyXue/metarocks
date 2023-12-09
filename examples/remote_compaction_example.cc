3c77497d9173bd5c18f80c5cc29637fa165e3fb4
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2023-12-09 12:58:20 -0600

    Use namespaces, add sstable stats

--- examples/remote_compaction_example.cc
+++ examples/remote_compaction_example.cc
@@ -1,73 +1,112 @@
 #include <iostream>
 
-#include "rocksdb/db.h"
 #include "rocksdb/compaction_service.h"
+#include "rocksdb/db.h"
 #include "rocksdb/options.h"
+#include "rocksdb/rocksdb_namespace.h"
 #include "rocksdb/slice.h"
 
-static std::string Key(int i) {
+using ROCKSDB_NAMESPACE::ColumnFamilyMetaData;
+using ROCKSDB_NAMESPACE::CompactRangeOptions;
+using ROCKSDB_NAMESPACE::DB;
+using ROCKSDB_NAMESPACE::Env;
+using ROCKSDB_NAMESPACE::EventListener;
+using ROCKSDB_NAMESPACE::ExternalCompactionService;
+using ROCKSDB_NAMESPACE::FlushOptions;
+using ROCKSDB_NAMESPACE::LiveFileMetaData;
+using ROCKSDB_NAMESPACE::Options;
+using ROCKSDB_NAMESPACE::ReadOptions;
+using ROCKSDB_NAMESPACE::Statistics;
+using ROCKSDB_NAMESPACE::Status;
+using ROCKSDB_NAMESPACE::TablePropertiesCollectorFactory;
+using ROCKSDB_NAMESPACE::WriteOptions;
+
+using namespace std;
+
+static string Key(int i) {
   char buf[100];
   snprintf(buf, sizeof(buf), "key%06d", i);
-  return std::string(buf);
+  return string(buf);
 }
 
-std::string kDBPath = "/tmp/rocksdb_remote_test";
+string kDBPath = "/tmp/rocksdb_remote_test";
 
-static void PrintStats(rocksdb::DB* db) {
-  std::string stats;
+static void PrintStats(DB* db) {
+  string stats;
   db->GetProperty("rocksdb.stats", &stats);
-  std::cout << "Stats: " << stats << std::endl;
+  cout << "Stats: " << stats << endl;
+}
+
+static void PrintSSTableCounts(DB* db) {
+  vector<LiveFileMetaData> metadata;
+  db->GetLiveFilesMetaData(&metadata);
+  cout << "SSTable Counts: " << metadata.size() << endl;
+
+  ColumnFamilyMetaData meta;
+  db->GetColumnFamilyMetaData(&meta);
+
+  vector<std::vector<rocksdb::SstFileMetaData>::size_type>
+      sstable_files_by_level;
+  for (const auto& level : meta.levels) {
+    sstable_files_by_level.push_back(level.files.size());
+  }
+
+  cout << "SSTable Files by Level: ";
+  for (vector<int>::size_type i = 0; i < sstable_files_by_level.size(); i++) {
+    cout << sstable_files_by_level[i] << " ";
+  }
+  cout << endl;
 }
 
 int main(int argc, char** argv) {
-  std::cout << "Remote Compaction Demo." << std::endl;
+  cout << "Remote Compaction Demo." << endl;
 
   // Create DB
-  std::cout << "Creating db at " << kDBPath << std::endl;
+  cout << "Creating db at " << kDBPath << endl;
 
-  rocksdb::DB* db;
+  DB* db;
 
-  rocksdb::Env* env = rocksdb::Env::Default();
-  rocksdb::Options primary_options;  // for client
+  Env* env = Env::Default();
+  Options primary_options;  // for client
   {
-    rocksdb::Options options;
+    Options options;
     options.env = env;
     options.create_if_missing = true;
     options.fail_if_options_file_error = true;
-    options.disable_auto_compactions = true; // do not compact on client
+    options.disable_auto_compactions = true;  // do not compact on client
     // options.compaction_service will be set later
     primary_options = options;
   }
-  std::shared_ptr<rocksdb::Statistics> primary_statistics;
+  shared_ptr<Statistics> primary_statistics;
   primary_statistics = ROCKSDB_NAMESPACE::CreateDBStatistics();
 
   primary_options.statistics = primary_statistics;
 
   // Create External Compaction Service
 
-  std::cout << "Creating compaction service" << std::endl;
+  cout << "Creating compaction service" << endl;
 
-  rocksdb::Options compactor_options;
+  Options compactor_options;
   {
-    rocksdb::Options options;
+    Options options;
     options.env = env;
-    options.create_if_missing = false; // secondary
+    options.create_if_missing = false;  // secondary
     options.fail_if_options_file_error = true;
     compactor_options = options;
   }
 
-  std::shared_ptr<rocksdb::ExternalCompactionService> compaction_service;
-  rocksdb::ExternalCompactionService* cs;
+  shared_ptr<ExternalCompactionService> compaction_service;
+  ExternalCompactionService* cs;
   {
-    std::string db_path = kDBPath;
-    std::shared_ptr<rocksdb::Statistics> compactor_statistics =
+    string db_path = kDBPath;
+    shared_ptr<Statistics> compactor_statistics =
         ROCKSDB_NAMESPACE::CreateDBStatistics();
-    std::vector<std::shared_ptr<rocksdb::EventListener>> listeners;
-    std::vector<std::shared_ptr<rocksdb::TablePropertiesCollectorFactory>>
+    vector<shared_ptr<EventListener>> listeners;
+    vector<shared_ptr<TablePropertiesCollectorFactory>>
         table_properties_collector_factories;
 
     compaction_service =
-        std::make_shared<ROCKSDB_NAMESPACE::ExternalCompactionService>(
+        make_shared<ROCKSDB_NAMESPACE::ExternalCompactionService>(
             db_path, compactor_options, compactor_statistics, listeners,
             table_properties_collector_factories);
     cs = compaction_service.get();
@@ -79,129 +118,126 @@ int main(int argc, char** argv) {
 
   // Write some values
 
-  rocksdb::Status s = rocksdb::DB::Open(primary_options, kDBPath, &db);
+  Status s = DB::Open(primary_options, kDBPath, &db);
   assert(s.ok());
 
   // TEST: Write then read a key-value pair
-  std::cout << "TEST: write-read one value" << std::endl;
+  cout << "TEST: write-read one value" << endl;
 
-  s = db->Put(rocksdb::WriteOptions(), "key1", "value1");
+  s = db->Put(WriteOptions(), "key1", "value1");
   assert(s.ok());
   {
-    std::string value;
-    s = db->Get(rocksdb::ReadOptions(), "key1", &value);
+    string value;
+    s = db->Get(ReadOptions(), "key1", &value);
     assert(s.ok());
 
     assert(value == "value1");
   }
 
+  PrintSSTableCounts(db);
+
   // TEST: Time to write lots of values
 
-  std::cout << "TEST: insert many values x10" << std::endl;
+  cout << "TEST: insert many values x10" << endl;
 
   const int BATCH_SIZE = 100000;
-  const int BATCHES = 2;
+  const int BATCHES = 5;
   for (int n = 0; n < BATCHES; n++) {
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = chrono::high_resolution_clock::now();
 
     for (int i = BATCH_SIZE * (n - 1); i < BATCH_SIZE * (n); i++) {
-      std::string key = "key" + std::to_string(i);
-      std::string value = "value" + std::to_string(i);
-      s = db->Put(rocksdb::WriteOptions(), key, value);
+      string key = "key" + to_string(i);
+      string value = "value" + to_string(i);
+      s = db->Put(WriteOptions(), key, value);
       assert(s.ok());
     }
 
-    s = db->Flush(rocksdb::FlushOptions());
+    s = db->Flush(FlushOptions());
     assert(s.ok());
 
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end - start;
-    std::cout << "Time: " << diff.count() << " s" << std::endl;
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> diff = end - start;
+    cout << "Time: " << diff.count() << " s" << endl;
 
-    
+    PrintSSTableCounts(db);
   }
 
   // SETUP: insert some test values to look for later
-  std::cout << "Inserting some more values" << std::endl;
+  cout << "Inserting some more values" << endl;
 
   for (int i = 0; i < 20; i++) {
     for (int j = 0; j < 10; j++) {
       int key_id = i * 10 + j;
-      s = db->Put(rocksdb::WriteOptions(), Key(key_id),
-                  "value" + std::to_string(key_id));
+      s = db->Put(WriteOptions(), Key(key_id), "value" + to_string(key_id));
       assert(s.ok());
     }
-    s = db->Flush(rocksdb::FlushOptions());
+    s = db->Flush(FlushOptions());
     assert(s.ok());
   }
 
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
       int key_id = i * 20 + j * 2;
-      s = db->Put(rocksdb::WriteOptions(), Key(key_id),
-                  "value_new" + std::to_string(key_id));
+      s = db->Put(WriteOptions(), Key(key_id), "value_new" + to_string(key_id));
       assert(s.ok());
     }
-    s = db->Flush(rocksdb::FlushOptions());
+    s = db->Flush(FlushOptions());
     assert(s.ok());
   }
-  
 
-  std::cout << "Verifying values are correct" << std::endl;
+  cout << "Verifying values are correct" << endl;
 
   for (int i = 0; i < 200; i++) {
-    std::string result;
-    db->Get(rocksdb::ReadOptions(), Key(i), &result);
+    string result;
+    db->Get(ReadOptions(), Key(i), &result);
     if (i % 2) {
-      assert(result == "value" + std::to_string(i));
+      assert(result == "value" + to_string(i));
     } else {
-      assert(result == "value_new" + std::to_string(i));
+      assert(result == "value_new" + to_string(i));
     }
   }
 
   // Perform compaction
 
+  PrintSSTableCounts(db);
+
   auto num = cs->GetCompactionNum();
 
-  std::cout << "Performing compaction " << num << std::endl;
+  cout << "Performing compaction " << num << endl;
 
-  rocksdb::CompactRangeOptions coptions;
+  CompactRangeOptions coptions;
   db->CompactRange(coptions, nullptr,
-                    nullptr);  // compact whole database, b/c nullptr is both
+                   nullptr);  // compact whole database, b/c nullptr is both
                               // first and last record
 
   num = cs->GetCompactionNum();
 
-  std::cout << "Compaction complete, now count is " << num << std::endl;
+  cout << "Compaction complete, now count is " << num << endl;
+
+  PrintSSTableCounts(db);
 
   // Verify values are correct
 
-  std::cout << "Verifying values are correct" << std::endl;
+  cout << "Verifying values are correct" << endl;
 
   for (int i = 0; i < 200; i++) {
-    std::string result;
-    db->Get(rocksdb::ReadOptions(), Key(i), &result);
+    string result;
+    db->Get(ReadOptions(), Key(i), &result);
     if (i % 2) {
-      assert(result == "value" + std::to_string(i));
+      assert(result == "value" + to_string(i));
     } else {
-      assert(result == "value_new" + std::to_string(i));
+      assert(result == "value_new" + to_string(i));
     }
   }
 
   // Statistics
+  // PrintStats(db)
 
-  
-
-
-  std::cout << "Done with main" << std::endl;
+  cout << "Done with main" << endl;
 
   // clean up db in temp folder
   delete db;
   rocksdb::DestroyDB(kDBPath, primary_options);
 
-  // ROCKSDB_NAMESPACE::port::InstallStackTraceHandler();
-  //::testing::InitGoogleTest(&argc, argv);
-  // RegisterCustomObjects(argc, argv);
-  // return RUN_ALL_TESTS();
   return 0;
 }
